9651eeef013b40aff2357d41c48332502a84e2b3
MartvdZalm <martvanderzalm@gmail.com>
2025-04-27 23:02:26 +0200

    AudioService

--- src/app/services/audio.service.ts
+++ src/app/services/audio.service.ts
@@ -6,16 +6,9 @@ import { Injectable } from '@angular/core';
 export class AudioService {
   private audioContext: AudioContext;
 
-  // public constructor() {
-  //   this.audioContext = new (window.AudioContext ||
-  //     (window as any).webkitAudioContext)();
-  // }
-
   public constructor() {
-    // Type the `window` object more specifically, as `window` can be any object in TypeScript.
     const audioContextConstructor = window.AudioContext || (window as { webkitAudioContext?: AudioContext }).webkitAudioContext;
     
-    // Now safely create an AudioContext instance.
     if (audioContextConstructor) {
       this.audioContext = new audioContextConstructor();
     } else {
@@ -28,8 +21,8 @@ export class AudioService {
       const response = await fetch(url);
       const arrayBuffer = await response.arrayBuffer();
       const audioBuffer = await this.audioContext.decodeAudioData(arrayBuffer);
-
       const source = this.audioContext.createBufferSource();
+      
       source.buffer = audioBuffer;
       source.connect(this.audioContext.destination);
       source.start(0);
